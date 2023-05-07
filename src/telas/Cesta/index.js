3db4f624bb294f82a945322f3c27dc31d5b7974f
MiguelAmoedo <miguelbombs@gmail.com>
2023-05-07 16:30:29 -0400

    2.0

---
+++ src/telas/Cesta/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, View, ScrollView } from 'react-native';
+
+import Topo from './componentes/Topo';
+import Detalhes from './componentes/Detalhes';
+import Itens from './componentes/items';
+
+export default function Cesta({ topo, detalhes , itens }) {
+  return <ScrollView>
+    <Topo {...topo} />
+
+    <View style={estilos.cesta}>
+      <Detalhes {...detalhes} />
+      <Itens {...itens} />
+    </View>
+  </ScrollView>
+}
+
+const estilos = StyleSheet.create({
+  cesta: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+});
